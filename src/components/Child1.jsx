6d6781c235a90cd8733ea85ac08b094e81876dcf
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-12 16:45:25 +0530

    Add Child1 component with selection handling

--- src/components/Child1.jsx
+++ src/components/Child1.jsx
@@ -1,1 +1,15 @@
 
+function Child1({setSelectedOption}) {
+    function handleSelection() {
+        setSelectedOption("Option 1")
+    }
+
+  return (
+    <div className='child1'>
+      <h2>Child Component1</h2>
+      <button onClick={handleSelection}>Option 1</button>
+    </div>
+  )
+}
+
+export default Child1
