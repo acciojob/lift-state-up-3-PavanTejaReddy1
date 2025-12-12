2b8f21ceef5995fd4c0f058d9f97bfcaeec3ea0f
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-12-12 16:45:44 +0530

    Update Child2.jsx

--- src/components/Child2.jsx
+++ src/components/Child2.jsx
@@ -1,1 +1,16 @@
 
+function Child2({setSelectedOption}) {
+    function handleSelection() {
+        setSelectedOption("Option 2")
+    }
+
+  return (
+    <div className='child2'>
+        <h2>Child Component 2</h2>
+        <button onClick={handleSelection}>Option 2</button>
+    </div>
+  )
+}
+
+export default Child2
+
